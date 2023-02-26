bff1c8b4b4194fe463c9e2d947ea7af23e5df67a
ZaripovT <timurfromcanada@gmail.com>
2023-02-26 22:12:13 +0500

    Change MyList<T>.cs

--- MyList<T>.cs
+++ MyList<T>.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace GenericsMyList
+{
+    class MyList<T>
+    {
+        protected List<T> arrayList = new List<T>();
+
+        public void AddElement(T t)
+        {
+            arrayList.Add(t);
+        }
+
+        public T Indexer(int index)
+        {
+            if (this.Count < index)
+            {
+                Console.WriteLine("The index was more than MyList has elements");
+                throw new Exception();
+            }
+            return arrayList[index - 1];
+        }
+
+        public int Count
+        {
+            get { return arrayList.Count; }
+        }
+    }
+}
